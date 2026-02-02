0d5ade9eb2d21b2f07d9d68bde27c8fc5bec04cf
kenta-yos <141031936+kenta-yos@users.noreply.github.com>
2026-02-02 09:38:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
+from st_gsheets_connection import GSheetsConnection
 
 st.set_page_config(page_title="ボルダリングセット情報", layout="wide")
 
